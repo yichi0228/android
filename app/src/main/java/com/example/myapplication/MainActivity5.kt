f98e0bba9ce93cf5c20f0ed6dd51c851886e9e9e
evelynlyn2003 <93966137+evelynlyn2003@users.noreply.github.com>
2022-12-20 10:12:44 +0800

    Update MainActivity5.kt

--- app/src/main/java/com/example/myapplication/MainActivity5.kt
+++ app/src/main/java/com/example/myapplication/MainActivity5.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
